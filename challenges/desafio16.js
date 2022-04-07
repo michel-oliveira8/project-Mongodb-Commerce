48e743e0bb615c40347da58bf977c8fa29c620d4
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-04-07 10:39:18 -0300

    desafio 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,6 @@
+db.produtos.updateOne({ nome: "Big Mac" }, {
+  $set: { ultimaModificacao: new Date() },
+});
+db.produtos.find({
+  ultimaModificacao: { $exists: true },
+}, { _id: false, nome: true });
